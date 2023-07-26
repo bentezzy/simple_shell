a6d9da83c17a06fdd783346c1affeef0208e0183
DANKAHIA <dankahia86@gmail.com>
2023-07-26 16:54:46 -0700

    exit_shell.c

--- exit_shell.c
+++ exit_shell.c
@@ -3,22 +3,21 @@
 /**
  * exec_line - finds builtins and commands
  *
- * @datash: data relevant (args)
- * @atoi: converts string to integer
+ * @data_rel: data relevant (args)
  * parsed command line
  * Return: 1 on success.
  */
-int exec_line(data_shell *datash)
+int exec_line(data_shell *data_rel)
 {
-	int (*builtin)(data_shell *datash);
+	int (*builtin)(data_shell *data_rel);
 
-	if (datash->args[0] == NULL)
+	if (data_rel->args[0] == NULL)
 		return (1);
 
-	builtin = get_builtin(datash->args[0]);
+	builtin = get_builtin(data_rel->args[0]);
 
 	if (builtin != NULL)
-		return (builtin(datash));
+		return (builtin(data_rel));
 
-	return (cmd_exec(datash));
+	return (cmd_exec(data_rel));
 }
